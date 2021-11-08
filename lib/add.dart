562fe614d9d607a476c0ea6c0c5946fba519fd3e
shravaninw <shravani.s@nativewit.in>
2021-11-08 19:02:49 +0530

    addition

---
+++ lib/add.dart
@@ -0,0 +1,8 @@
+// 1
+import 'package:annotations/annotations.dart';
+
+// 2
+part 'add.g.dart';
+
+@addition
+abstract class Add {}
---
+++ lib/add.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'add.dart';
+
+// **************************************************************************
+// AdditionGenerator
+// **************************************************************************
+
+class AddImpl extends Add {
+  int add(int a, int b) {
+    return a + b;
+  }
+}
